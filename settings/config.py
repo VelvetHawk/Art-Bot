91423e03754e9b594ccf591059bdc8f5b84381a3
Artem Semenov <artem@adamanthealth.com>
2023-06-21 18:42:16 +0300

    Rewrite: Removed old python code, added structure for new C++ implementation

--- settings/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-ITAD_KEY = os.getenv("ITAD_KEY")
-YOUTUBE_KEY = os.getenv("YOUTUBE_KEY")
-OWNER_ID = os.getenv("OWNER_ID")
-FFMPEG_PATH = os.getenv("FFMPEG_PATH")
